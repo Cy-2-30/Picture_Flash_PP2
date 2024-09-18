270cb962b22dbd4315c1b1149224fbeb6f547625
Cy-2-30 (Dee) <lebulicynthia@gmail.com>
2024-09-18 01:29:28 +0000

    add event funtion to the radio button options and start button

--- script.js
+++ script.js
@@ -20,3 +20,32 @@ let currentPlayerTurn = 1;
 let scorePoints = 0;
 let movesLeft = 20;
 
+// EVENT LISTERNER
+gameMode.array.forEach(radio => {
+    radio.addEventListener('change', updateGameMode);
+});
+
+function updateGameMode(){
+    gameMode = document.querySelector('input[name="game_mode"]:checked').value;
+    if (gameMode === 'another_player'){
+        playerTwoDiv.style.display = 'block';
+    } else {
+        playerTwoDiv.style.display ='none';
+    }
+}
+
+playButton.addEventListener('click', playButton);
+
+function playButton(){
+    player1Name = playerOneNameInput.value || 'Player 1';
+    player2Name = gameMode === 'another_player' ? (playerTwoNameInput.value || 'Player 2') : 'Computer';
+
+    // HIDE THE NAME INPUT SCREEN AND SHOW GAME SCREEN INFO
+    gameInfo.style.display = 'block';
+    gameSetup.style.display = 'none';
+
+    //UPDATE THE GAME STATISTIC SCREEN
+    updatePlayerTurn();
+    updateScore();
+    updatePlayerMoves();
+}
