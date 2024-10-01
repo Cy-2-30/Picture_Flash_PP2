5595a9d09d29484d9dbffb00ade7126fd1255770
Cy-2-30 (Dee) <lebulicynthia@gmail.com>
2024-10-01 15:27:15 +0000

    adding uncommited changes to change the login account

--- script.js
+++ script.js
@@ -3,9 +3,11 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     // PLAYER DETAILS AND MODE
-    const gameSetupDiv = document.getElementById('game_setup');
     const gameModeDiv = document.getElementById('mode_setup');
     const playerNamesDiv = document.querySelector('.player_name_input');
+    const gameInfo = document.getElementById('game_info');
+    const gameStatus = document.getElementById('game_status');
+    const gameBoard = document.getElementById('game_board');
     //const playerTwoDiv = document.getElementsByID('player2_names');
     // the queryselector for next button will decide which to display
 
@@ -85,63 +87,4 @@ document.addEventListener('DOMContentLoaded', () => {
         gameBoard.style.display = 'none';  
     });
 
-
-}
-
-
-/// TO BE CORRECTED !!!!
-// GAME STATS
-const gameInfo = document.getElementById('game_info');
-
-const gameStatus = document.getElementById('game_status');
-const gameBoard = document.getElementById('game_board');
-
-
-const playButton = document.getElementById('start_game');
-const quitButton = document.getElementById('quit_game');
-const playerTurn = document.getElementById('current_turn');
-const playMoves = document.getElementById('moves');
-const gameHint = document.getElementById('hints');
-const score = document.getElementById('score');
-
-// GAME BOARD 
-
-let currentPlayerTurn = 1;
-let scorePoints = 0;
-let movesLeft = 20;
-
-// EVENT LISTERNER
-//gameModeRadios.array.forEach(radio => {
-  //  radio.addEventListener('change', updateGameMode);
-//});
-
-
-//playButton.addEventListener('click', playGameButton);
-
-
-
-
-gameHint.addEventListener('click', showHint);
-
-function showHints(){
-    console.log('Hint clicked!');
-}
-
-function updateScorePoints(){
-    score.textContent = "Score: ${scorePoints}";
-}
-
-function updateMovesLeft(){
-    playMoves.textContent = "Moves left: ${movesLeft}";
-}
-
-// RESET THE GAME STATISTICS
-function resetGame(){
-    score = 0;
-    movesLeft = 20;
-    currentPlayerTurn = 1;
-    updateScorePoints();
-    updateMovesLeft();
-    updatePlayerTurn();
-}
-
+});
